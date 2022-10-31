96036a021572a8b06f5f35ad08dbabc227cfebd1
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2022-10-31 17:40:48 +0100

    body read

--- src/push_listener.rs
+++ src/push_listener.rs
@@ -1,3 +1,5 @@
+use httparse::Header;
+
 use crate::options::ContainerOptions;
 use std::io::{Error, Read};
 use std::net::TcpListener;
@@ -32,11 +34,10 @@ fn parse_stream(listener: Arc<TcpListener>) -> Result<bool, Box<dyn std::error::
 
     let mut final_buffer = vec![];
     loop {
-        let mut buf = vec![0u8; 1000];
+        let mut buf = [0u8; 10];
         let read = stream.read(&mut buf)?;
 
-        buf.shrink_to(read);
-        final_buffer.append(&mut buf);
+        final_buffer.extend_from_slice(&buf[0..read]);
 
         let mut headers = [httparse::EMPTY_HEADER; 64];
         let mut req = httparse::Request::new(&mut headers);
@@ -46,13 +47,50 @@ fn parse_stream(listener: Arc<TcpListener>) -> Result<bool, Box<dyn std::error::
             Err(_) => return Ok(false),
         };
         if status.is_complete() {
-            println!("{req:?}");
-
             if req.method == Some("POST") {
-                return Ok(true);
+                break;
             } else {
                 return Ok(false);
             }
         }
+    }
+
+    let mut headers = [httparse::EMPTY_HEADER; 64];
+    let mut request = httparse::Request::new(&mut headers);
+
+    request.parse(&final_buffer[..])?;
+
+    let mut found_header = false;
+    let mut content_length: usize = 0;
+    for header in request.headers {
+        if header.name == "Content-Length" {
+            found_header = true;
+            content_length = String::from_utf8(header.value.to_vec())?.parse()?;
+            break;
+        }
+    }
+    if found_header {
+        let str = String::from_utf8(final_buffer.clone())?;
+
+        let body_start = match str.find("\r\n\r\n") {
+            Some(index) => index,
+            None => {
+                return Ok(false);
+            }
+        };
+        let mut body = String::from(str.split_at(body_start+4).1);
+
+        if body.len() != content_length {
+            let mut rest_of_body_buffer = vec![0u8; content_length - body.len()];
+            stream.read_exact(&mut rest_of_body_buffer)?;
+
+            body = format!("{body}{}", String::from_utf8(rest_of_body_buffer)?);
+        }
+
+        println!("body: \n{body}");
+
+        Ok(true)
+    } else {
+        Ok(false)
     }
 }
